0c9609b39479b64574713985ca39e1044f706d60
Sergei Fedorov <sergei.a.fedorov@gmail.com>
2025-12-05 13:53:08 +0100

    Add groups listing to FUSE filesystem
    - Add /groups/ directory listing groups and supergroups
    - Groups with username display username, otherwise display title (sanitised)
    - Each group has .info file with title, username (if public), and type
    - Group mtime set to last message timestamp

--- include/fuse/constants.hpp
+++ include/fuse/constants.hpp
@@ -7,6 +7,7 @@ namespace tgfuse {
 // Directory names for the virtual filesystem
 inline constexpr std::string_view kUsersDir = "users";
 inline constexpr std::string_view kContactsDir = "contacts";
+inline constexpr std::string_view kGroupsDir = "groups";
 inline constexpr std::string_view kInfoFile = ".info";
 inline constexpr std::string_view kSelfSymlink = "self";
 
